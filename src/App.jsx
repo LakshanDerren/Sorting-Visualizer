1204df3abbff1af3eeee4fddf6191eb05d4d225c
LakshanDerren <isuru21256@gmail.com>
2025-01-29 15:48:10 +0530

    merge sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [array, setArray] = useState([]);
+  const [isSorting,setIsSorting] = useState(false)
+
+  const resetArray = () => {
+    const tempArray = []
+    for(let i = 0; i<100; i++){
+      const randomV = Math.floor(Math.random() * 100) + 10
+      tempArray.push(randomV)
+    }    
+    setArray(tempArray);
+  }
+
+
+  const mergeSort = async (arr, start, end) => {
+    if (end - start <= 1) return arr.slice(start, end); // Base case
+  
+    const mid = Math.floor((start + end) / 2);
+    const left = await mergeSort(arr, start, mid);
+    const right = await mergeSort(arr, mid, end);
+  
+    return await merge(left, right, start, end);
+  };
+  
+
+  const merge = async (arr1,arr2,start,end)=>{
+    let returnArry = []
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+      if(arr1[i] < arr2[j]){
+        returnArry.push(arr1[i]);
+        i++
+      }else{
+        returnArry.push(arr2[j])
+        j++
+      }
+    }
+    while(i < arr1.length){
+      returnArry.push(arr1[i]);
+      i++
+    }
+    while(j < arr2.length){
+      returnArry.push(arr2[j]);
+      j++
+    }
+    await new Promise((resolve)=>{setTimeout(resolve,300)})
+    setArray((preArr)=>[
+      ...preArr.slice(0,start),
+      ...returnArry,
+      ...preArr.slice(end)
+    ])
+    return returnArry;
+  }
+
+  const handleMergeSort = async () => {
+    setIsSorting(true);
+    await mergeSort(array, 0, array.length);
+    setIsSorting(false);
+  };
+
+
+  useEffect(()=>{
+    resetArray()
+  },[])
   return (
-    <>
-    </>
+    <div className='body'>
+      <div className="btnbar">
+        <button disabled={isSorting} onClick={()=>resetArray()}>New</button>
+        <button disabled={isSorting} onClick={()=>handleMergeSort()}>MergeSort</button>
+        <button disabled={isSorting} >kandy</button>
+        <button disabled={isSorting}>click</button>
+        <button disabled={isSorting}>thisWay</button>
+      </div>
+      <div className="graph">
+      {array.map((val, i) => (
+        <div className="line" style={{
+          height:`${val*3}px`,
+          transition: "all 0.2s ease-in-out"
+          }} key={i}>
+          
+        </div>
+      ))}
+      </div>
+    </div>
   )
 }
 
